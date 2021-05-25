d72c406541daa292944c2d7d39f163cf098a475c
Heather <hfolcot@gmail.com>
2021-05-25 22:19:31 +0100

    added UI

--- js/game.js
+++ js/game.js
@@ -13,17 +13,48 @@ class Game {
         this.snake = new Snake(10, 10);
         this.food = new Food();
         this.speed = 15;
-        this.state = 1;
+        this.state = 0;
         this.frame = 0;
         this.points = 0;
         new InputHandler();
     }
+    init(){
+        this.snake = new Snake(10, 10);
+        this.food = new Food();
+        this.speed = 15;
+        this.frame = 0;
+        this.points = 0;
+    }
     update(ctx, canvas) {
+        if(this.state == 0){
+            ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+            ctx.fillRect(0, 0, canvas.height, canvas.width);
+            let startMessage = new Message(canvas.width/2, canvas.height/2, 'bold 25px Arial', "Welcome to Snake!", "white");
+            startMessage.draw(ctx);
+            let startText = new Message(canvas.width/2, canvas.height/2 + 30, 'bold 18px Arial', "Press space to start", "white");
+            startText.draw(ctx);  
+        }
         if (this.state === 1) {
             this.snake.update(this, canvas);
         }
         this.food.draw(ctx);
         this.snake.draw(ctx);
+        if(this.state === 2){
+            ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+            ctx.fillRect(0, 0, canvas.height, canvas.width);
+            let pauseText = new Message(canvas.width/2, canvas.height/2, 'bold 25px Arial', "Paused", "white");
+            pauseText.draw(ctx);
+        }
+        if(this.state === 3){
+            ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+            ctx.fillRect(0, 0, canvas.height, canvas.width);
+            let gameOverMessage = new Message(canvas.width/2, canvas.height/2, 'bold 25px Arial', "Game Over", "white");
+            gameOverMessage.draw(ctx);
+            let scoreText = new Message(canvas.width/2, canvas.height/2 + 30, 'bold 18px Arial', "Your Score: " + game.points, "white");
+            scoreText.draw(ctx);        
+            let restartText = new Message(canvas.width/2, canvas.height/2 + 60, 'bold 18px Arial', "Press space to restart", "white");
+            restartText.draw(ctx);        
+        }
 
     }
 }
--- js/input.js
+++ js/input.js
@@ -1,6 +1,7 @@
 class InputHandler {
     constructor(){
         document.addEventListener('keydown', function(e) {
+            console.log(e)
             // Prevent snake from turning in opposite direction into itself
             if(e.key === 'ArrowLeft' && game.snake.direction != 'r'){
                 game.snake.direction = 'l';
@@ -13,6 +14,19 @@ class InputHandler {
             }
             if(e.key === 'ArrowDown' && game.snake.direction != 'u'){
                 game.snake.direction = 'd';
+            }
+
+            // Pause
+            if(e.key ==='p' && game.state == 1){
+                game.state = 2;
+            } else if(e.key ==='p' && game.state === 2) {
+                game.state = 1;
+            }
+
+            // Start/Restart Game
+            if(e.code == 'Space' && [0, 3].indexOf(game.state) != -1){
+                game.init();
+                game.state = 1;
             }
         })
     }
